9c6c8e0ae28aeaf7f6dbca5f4db8a85eea7995e3
RemiSaurel <r.saurel5@gmail.com>
2025-02-01 09:50:17 +0100

    fix: 3.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
+import exos.Concert
 import exos.exo21
 import exos.exo22
+import java.util.*
 
 fun main() {
     // exo21()
     // exo22()
+
+    val concert1 = Concert("H JeuneCrack", Date(), "Le Bikini")
+    val concert2 = Concert("Mairo", Date(), "Zenith Toulouse")
+
+    println(concert1.description())
+    println(concert2.description())
+
 }
