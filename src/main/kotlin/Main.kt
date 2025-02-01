87f55f108679e76df67f18690400ba7f8a10f727
RemiSaurel <r.saurel5@gmail.com>
2025-02-01 09:41:50 +0100

    fix: 1.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,33 @@
 fun main() {
-
-    val bikini = "Le Bikini"
-    val zenith = "Zenith Toulouse"
-    val rex = "Le Rex"
-
-    println("$bikini - $zenith - $rex")
+    println(checkGenre(Genre.RAP))// Genre rap supporté
+    println(checkGenre(Genre.TECHNO)) // Genre techno supporté
+    println(checkGenre(Genre.ROCK)) // Genre rock non supporté
 }
+
+/**
+ * Vérifie si les genres "rap" et "techno" sont supportés
+ * @param genre le genre à vérifier
+ */
+fun checkGenre(genre: String): String {
+    return if (genre == "rap" || genre == "techno") {
+        "Genre $genre supporté"
+    } else {
+        "Genre $genre non supporté"
+    }
+}
+
+// BONUS
+enum class Genre {
+    RAP,
+    TECHNO,
+    ROCK
+}
+
+fun checkGenre(genre: Genre): String {
+    return if (genre == Genre.RAP || genre == Genre.TECHNO) {
+        "Genre $genre supporté"
+    } else {
+        "Genre $genre non supporté"
+    }
+}
+
