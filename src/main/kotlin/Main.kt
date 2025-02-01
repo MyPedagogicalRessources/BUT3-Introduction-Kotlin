a48dad8305d6e6e844ecfa72aa3fd9c18ad96ec6
RemiSaurel <r.saurel5@gmail.com>
2025-02-01 09:46:11 +0100

    fix: 2.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import exos.exo21
+import exos.exo22
 
 fun main() {
-    exo21()
+    // exo21()
+    exo22()
 }
--- src/main/kotlin/exos/exo2.kt
+++ src/main/kotlin/exos/exo2.kt
@@ -9,3 +9,16 @@ fun exo21() {
         println(rappeur)
     }
 }
+
+fun exo22() {
+    val salles = mutableMapOf(
+        "Le Bikini" to 1500,
+        "Zenith Toulouse" to 11000,
+        "Le Rex" to 800
+    )
+    salles["Nouvelle salle"] = 5000
+
+    for ((salle, capacite) in salles) { // Destructuring
+        println("$salle - $capacite")
+    }
+}
